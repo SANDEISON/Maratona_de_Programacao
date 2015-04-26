160619a6cf45908e92d52c7c17b7688ac9db43b4
Sandeison Fernandes <sandeison_san@hotmail.com>
2015-04-26 13:38:15 -0300

    uva 11608

--- 01_Arrays/UVA_11608/main.cpp
+++ 01_Arrays/UVA_11608/main.cpp
@@ -8,11 +8,14 @@ int main ( )
 
     while (s >= 0){
         int soma = 0, probCriado[12], probNecessario[12];
+
         cin >> s;
+
 
         if ((s < 0) or (s > 100)){
             break;
         }
+        cout << "Case "<< cont <<":" <<endl;
 
         for(int i=0; i < 12; i++){
             cin>>probCriado[i];
@@ -22,26 +25,19 @@ int main ( )
             cin>>probNecessario[i];
         }
 
-        cout << "Case "<< cont <<":" <<endl;
-
-
+        soma=s;
         for(int i=0; i < 12; i++){
-            if(i>0){
-                soma = probCriado[i-1]+ probCriado[i];
-
-            }else{
-               soma = s+probCriado[0];
-            }
-            soma -= probNecessario[i];
-            if (soma>=0){
+            if (soma>=probNecessario[i]){
                 cout<<"No problem! :D"<<endl;
+                soma-=probNecessario[i];
             }else{
                 cout<<"No problem. :("<<endl;
             }
-
+            soma += probCriado[i];
         }
-        cont++;
 
+
+        cont++;
     }
 
 
