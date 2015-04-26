dfabf10f44d95ffc4198793c6c93c322e72466f4
Sandeison Fernandes <sandeison_san@hotmail.com>
2015-04-26 17:19:57 -0300

    array

--- 01_Arrays/UVA_466/main.cpp
+++ 01_Arrays/UVA_466/main.cpp
@@ -4,6 +4,16 @@ using namespace std;
 
 int main ( )
 {
+    int cont=1;
+    while (1){
+        int n;
+        cin >> n;
+        char entrada[n][n];
 
+
+
+
+        cout << cont << endl;
+    }
     return 0 ;
 }
--- 01_Arrays/UVA_755/main.cpp
+++ 01_Arrays/UVA_755/main.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main ( )
 {
     int n =0;
-    list<char> telefones;
+
 
 
 
