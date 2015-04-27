0fa42842db6457da300f866b0e7a68f1626ad14b
Sandeison Fernandes <sandeison_san@hotmail.com>
2015-04-26 22:45:46 -0300

    FINAL

--- 01_Arrays/UVA_466/main.cpp
+++ 01_Arrays/UVA_466/main.cpp
@@ -1,19 +1,102 @@
 #include <iostream>
-
+#include <vector>
+#include <algorithm>
 using namespace std;
 
-int main ( )
+template <typename T>
+void rotate(vector<vector<T> > &v)
 {
-    int cont=1;
-    while (1){
-        int n;
-        cin >> n;
-        char entrada[n][n];
+    size_t n = v.size();
+    for (size_t r = 0; r < n / 2; ++r)
+        for (size_t c = 0; c < (n + 1) / 2; ++c)
+        {
+            T temp = v[r][c];
+            v[r][c] = v[n - 1 - c][r];
+            v[n - 1 - c][r] = v[n - 1 - r][n - 1 - c];
+            v[n - 1 - r][n - 1 - c] = v[c][n - 1 - r];
+            v[c][n - 1 - r] = temp;
+        }
+}
 
 
+template <typename T>
+void reflect(vector<vector<T> > &v)
+{
+    size_t n = v.size();
+    for (size_t r = 0; r < n / 2; ++r)
+        for (size_t c = 0; c < n; ++c)
+            swap(v[r][c], v[n - 1 - r][c]);
+}
 
 
-        cout << cont << endl;
+void compare(vector<vector<char> > &pattern,
+             const vector<vector<char> > &newPattern,
+             int &ro,
+             int &re)
+{
+
+    while (re < 2)
+    {
+        while (ro < 4)
+        {
+            if (pattern == newPattern)
+                return;
+
+            rotate(pattern);
+            ++ro;
+        }
+
+        ro = 0;
+        reflect(pattern);
+        ++re;
     }
-    return 0 ;
+}
+
+int main()
+{
+    int T = 1;
+    int n;
+    while (cin >> n)
+    {
+        vector<vector<char> > pattern(n, vector<char>(n)),
+                              newPattern(n, vector<char>(n));
+        for (int r = 0; r < n; ++r)
+        {
+            for (int c = 0; c < n; ++c)
+                cin >> pattern[r][c];
+            for (int c = 0; c < n; ++c)
+                cin >> newPattern[r][c];
+        }
+
+        int ro = 0, re = 0;
+        compare(pattern, newPattern, ro, re);
+
+        cout << "Pattern " << T++ << " was ";
+
+        if (re == 2)
+            cout << "improperly transformed.";
+        else if (re == 0)
+        {
+            if (ro == 0)
+                cout << "preserved.";
+            else
+                cout << "rotated "
+                     << 90 * ro
+                     << " degrees.";
+        }
+        else
+        {
+            cout << "reflected vertically";
+            if (ro == 0)
+                cout << ".";
+            else
+            {
+                cout << " and rotated "
+                     << 90 * ro
+                     << " degrees.";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
 }
--- 01_Arrays/UVA_755/main.cpp
+++ 01_Arrays/UVA_755/main.cpp
@@ -1,81 +1,126 @@
+#include <cstdlib>
 #include <iostream>
-#include<list>
 #include <string>
-#include<vector>
-
+#include <map>
+#include <algorithm>
 using namespace std;
 
-int main ( )
-{
-    int n =0;
+void HashInit(map<char, char>&);
+void Transform(map<char, char>&, string&);
+
+class StringSet{
+public:
+    StringSet(const int&);
+    ~StringSet(){ delete[] _data; }
+
+    void Push(const string&);
+    void Sort();
+    void Print()const;
+private:
+    string* _data;
+    const int _max;
+    int _top;
+};
 
 
+int main(int argc, char** argv){
+    map<char, char> char_hash;
+    HashInit(char_hash);
 
+    int testdata_num;
+    cin >> testdata_num;
 
-    cin >> n ;
-    cout << endl;
+    for(int eax = 0 ; eax < testdata_num ; ++eax){
+        if(eax){ cout << endl; }
 
-    for (int i=0 ; i<n ; i++){
+        int phone_num;
+        string phone;
+        cin >> phone_num;
+        StringSet phone_set(phone_num);
 
-        int qtNumero=0, k=0, cont=0;
-        string entrada;
-        char numero[7];
-
-        cin >> qtNumero ;
-
-        for (int j=0; j<qtNumero; j++){
-
-
-            cin >> entrada;
-            k=entrada.size();
-            for (int y=0 ; y < k ; y++){
-
-                if ((entrada[y]!= '-') && (entrada[y]!= 'Q')and (entrada[y]!= 'Z')){
-
-                  if (entrada[y]== 'A'||'B'||'C' ){
-                        numero[cont]='2';
-                   }else{
-                       if (entrada[y]== 'D'||'E'||'F' ){
-                            numero[cont]='3';
-                       }else{
-                           if (entrada[y]== 'G'||'H'||'I' ){
-                                numero[cont]='4';
-                           }else{
-                               if (entrada[y]== 'J'||'K'||'L' ){
-                                    numero[cont]='5';
-                               }else{
-                                   if (entrada[y]== 'M'||'N'||'O' ){
-                                        numero[cont]='6';
-                                   }else {
-                                       if (entrada[y]== 'P'||'R'||'S' ){
-                                            numero[cont]='7';
-                                       }else {
-                                           if (entrada[y]== 'T'||'U'||'V' ){
-                                                numero[cont]='8';
-                                           }else {
-                                                numero[cont]='9';
-                                           }
-                                       }
-                                   }
-
-                               }
-
-                           }
-                       }
-
-                    }
-
-
-
-                  cont++;
-
-
-
-                }
-
-             }
-           }
+        for(int i = 0 ; i < phone_num ; ++i){
+            cin >> phone;
+            Transform(char_hash, phone);
+            phone_set.Push(phone);
         }
 
-    return 0 ;
+        phone_set.Sort();
+        phone_set.Print();
+    }
+
+    return EXIT_SUCCESS;
+}
+
+
+
+void HashInit(map<char, char>& hash_table){
+    char second = '1';
+    for(char first = 'A' ; first < 'Q' ; ++first){
+        if( (first - 'A') % 3 == 0){ ++second; }
+        hash_table[first] = second;
+    }
+    for(char first = 'R' ; first < 'Z' ; ++first){
+         if( (first - 'Q') % 3 == 0){ ++second; }
+         hash_table[first] = second;
+    }
+    for(char i = '0' ; i <= '9' ; ++i){
+        hash_table[i] = i;
+    }
+}
+
+
+void Transform(map<char, char>& char_hash, string& msg){
+    string result = "000-0000";
+    int eax = 0;
+    for(int i = 0 ; i < msg.length() ; ++i){
+        if(msg[i] != '-'){
+            result[eax++] = char_hash[ msg[i] ];
+            if(eax == 3){ ++eax; }
+        }
+    }
+
+    msg = result;
+}
+
+
+
+StringSet::StringSet(const int& num)
+    :_max(num), _data(new string[num]), _top(-1){}
+
+void StringSet::Push(const string& origin){
+    ++_top;
+    try{
+        if( _top >= _max ){ throw; }
+    }catch(...){
+        cout << "Error at StringSet::Push(), over the limit!!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    _data[ _top ] = origin;
+}
+
+void StringSet::Sort(){
+    sort(_data, _data + _top + 1);
+}
+
+void StringSet::Print()const{
+    bool printed_flag = false;
+    string now("");
+    int same_num = 0;
+
+    for(int i = 0 ; i <= _top ; ++i){
+        if(_data[i] == now){ ++same_num; }
+        else{
+            if(same_num >= 2){
+                cout << now << ' ' << same_num << endl;
+                printed_flag = true;
+            }
+            now = _data[i];
+            same_num = 1;
+        }
+    }
+    if(same_num >= 2){
+        cout << now << ' ' << same_num << endl;
+        printed_flag = true;
+    }
+    if(!printed_flag){ cout << "No duplicates." << endl;}
 }
